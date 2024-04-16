d2945838998a5aa9817333a9aa0f8f993116c209
tayyib-saddique <tayyib14@gmail.com>
2024-04-16 11:40:31 +0100

    refactor

--- llm/openai.py
+++ llm/openai.py
@@ -10,21 +10,17 @@ client = OpenAI(
 
 def get_chat_completion(prompt, model="gpt-3.5-turbo"):
   
-   # Creating a message as required by the API
-   messages = [{"role": "user", "content": prompt}]
-  
-   # Calling the ChatCompletion API
-   response = client.ChatCompletion.create(
-       model=model,
-       messages=messages,
-       temperature=0,
-   )
+  response = client.chat.completions.create(
+      model=model,
+      messages=[
+        {"role": "system", "content": "You are a helpful assistant designed to summarise text."},
+        {"role": "user", "content": prompt}
+      ]
+      temperature = 0,
+    )
+  return response.choices[0].message["content"]
 
-   # Returning the extracted response
-   return response.choices[0].message["content"]
-
-response = get_chat_completion("Translate into Spanish: As a beginner data scientist, I'm excited to learn about OpenAI API!")
-
+response = get_chat_completion("Tell me more information about local policing in the North East of England")
 print(response)
 
 
