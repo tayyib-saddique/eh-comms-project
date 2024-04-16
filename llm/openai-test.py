5811a2a25e70a8c5c8aeb88d6197422e073b901e
tayyib-saddique <tayyib14@gmail.com>
2024-04-16 12:04:36 +0100

    Revert "removing newscatcherapi from openai"
    This reverts commit fe9baabf6465009904d720e5c7e2679a031712e9.

--- llm/openai-test.py
+++ llm/openai-test.py
@@ -10,11 +10,8 @@ client = OpenAI(
     api_key = os.environ.get("OPENAI_API_KEY"),
 )
 
-newscatcher = Newscatcher(
-      api_key= os.environ.get("NEWSCATCHER_API_KEY"), 
-  )
-
 def get_chat_completion(prompt, model="gpt-3.5-turbo"):
+  
   response = client.chat.completions.create(
       model=model,
       messages=[
@@ -24,7 +21,14 @@ def get_chat_completion(prompt, model="gpt-3.5-turbo"):
     )
   return response.choices[0].message["content"]
 
-def news_catcher(newscatcher):
+response = get_chat_completion("Tell me more information about local policing in the North East of England")
+print(response)
+
+def news_catcher():
+  newscatcher = Newscatcher(
+      api_key= os.environ.get("NEWSCATCHER_API_KEY"), 
+  )
+
   try:
       # [Get] Search By Author Request
       get_response = newscatcher.search.get(
@@ -42,13 +46,12 @@ def news_catcher(newscatcher):
       pprint(e.reason)
       pprint(e.round_trip_time)
 
+def main():
+    user_input = input()
 
-# user_input = input('Enter input')
-# response = get_chat_completion(user_input)
+    response = get_chat_completion(user_input)
+    print(response)
 
-response = get_chat_completion("Tell me more information about local policing in the North East of England")
-print(response)
-
-api_response = news_catcher()
-print(api_response)
+    api_response = news_catcher()
+    print(api_response)
 
